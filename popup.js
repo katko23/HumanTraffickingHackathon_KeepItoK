89782ff8b54e93167fa122323f8669dfe03f35c3
DarkWeider11 <boris.golis@isa.utm.md>
2023-11-25 14:20:55 +0200

    html css js

--- popup.js
+++ popup.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById("extractButton").addEventListener('click', function () {
     var procentElement = document.querySelector('.procent');
-    console.log(procentElement)
-    procentElement.style.visibility = 'visible';
+    procentElement.style.visibility = 'visible'
 
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       // Send a message to the content script to get HTML
       chrome.tabs.sendMessage(tabs[0].id, { action: 'get_html' }, function (response) {
         if (chrome.runtime.lastError) {
           console.error(chrome.runtime.lastError.message);
-          alert('Error: Could not establish connection. Please make sure the page is fully loaded.');
+          // alert('Error: Could not establish connection. Please make sure the page is fully loaded.');
           return;
         }
 
@@ -21,19 +20,13 @@ document.addEventListener('DOMContentLoaded', function () {
           },
           body: JSON.stringify({ html: response.html }),
         })
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
-            return response.json();
-          })
+          .then(response => response.json())
           .then(data => {
             // Display the JSON response on the popup
             alert(JSON.stringify(data));
           })
           .catch(error => {
             console.error('Error:', error);
-            alert('Error: Could not connect to the Python server.');
           });
       });
     });
